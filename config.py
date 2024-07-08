79ab900073afcf320c63f427f29027d90173a78f
leesviik <101561105+leesviik@users.noreply.github.com>
2024-07-08 22:42:13 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+CHANNEL_LINK = 'https://t.me/nelyton'
+
+admin_id = 1497154934
+
+CHAT_LINK = 'https://t.me/nelytonchat'
+
+OWNER_LINK = 'https://t.me/wapnely'
+
+a1_LEVEL = 0.5
+
+a2_LEVEL = 0.3
+
+a3_LEVEL = 0.1
+
+chats = [-1002197368039]
+
+BOT_TOKEN = '7398485985:AAHvbyehHDvBZXtiyHg1-mK6YNrngJNw8-8'
